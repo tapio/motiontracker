eead8e258e7b421195ee434b17f5215c37c436e1
Antti Virta <antti.virta@gmail.com>
2011-01-27 16:37:55 +0200

    Added edge detection to test GUI

--- main.cc
+++ main.cc
@@ -2,8 +2,8 @@
 #include "webcam.hh"
 
 #include "cv.h"
-#include "ml.h"
-#include "cxcore.hpp"
+//#include "ml.h"
+//#include "cxcore.hpp"
 #include "highgui.h"
 
 using namespace cv;
@@ -22,12 +22,14 @@ int main(int argc, char** argv)
         if (argc > 1) cap.open(string(argv[1]));
         else cap.open(0);
         Mat frame;
+        Mat edges;
         namedWindow("video",1);
         for (;;) {
             cap >> frame;
-            if(!frame.data) break;
-            GaussianBlur(frame,frame,Size(25,25),6,6);
-            imshow("video", frame);
+			cvtColor(frame, edges, CV_BGR2GRAY);
+			GaussianBlur(frame,frame,Size(15,15),1.5,1.5);
+			Canny(edges, edges,20,60,3);
+			imshow("video", edges);
             if(waitKey(30) >= 0) break;
         }
         return 0;
