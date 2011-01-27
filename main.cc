6da7f5017f65696aeb13437e30b8f51162bcaeea
Tapio Vierros <tapiovierros@gmail.com>
2011-01-27 16:44:58 +0200

    Fix whitespace.

--- main.cc
+++ main.cc
@@ -18,19 +18,20 @@ int main(int argc, char** argv)
 		std::cout << "no" << std::endl;
 	}
 
-        VideoCapture cap;
-        if (argc > 1) cap.open(string(argv[1]));
-        else cap.open(0);
-        Mat frame;
-        Mat edges;
-        namedWindow("video",1);
-        for (;;) {
-            cap >> frame;
-			cvtColor(frame, edges, CV_BGR2GRAY);
-			GaussianBlur(edges,edges,Size(15,15),1.5,1.5);
-			Canny(edges, edges,20,60,3);
-			imshow("video", edges);
-            if(waitKey(30) >= 0) break;
-        }
-        return 0;
+	VideoCapture cap;
+	if (argc > 1) cap.open(string(argv[1]));
+	else cap.open(0);
+
+	Mat frame;
+	Mat edges;
+	namedWindow("video",1);
+	for (;;) {
+		cap >> frame;
+		cvtColor(frame, edges, CV_BGR2GRAY);
+		GaussianBlur(edges, edges, Size(15,15), 1.5, 1.5);
+		Canny(edges, edges, 20, 60, 3);
+		imshow("video", edges);
+		if(waitKey(30) >= 0) break;
+	}
+	return 0;
 }
