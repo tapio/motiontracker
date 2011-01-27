bebe05c1d09aa3ab3c66ad22f33ef616623fae3f
Antti Virta <antti.virta@gmail.com>
2011-01-27 16:19:22 +0200

    Added small GUI

--- main.cc
+++ main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "webcam.hh"
 
+#include "cv.h"
+#include "ml.h"
+#include "cxcore.hpp"
+#include "highgui.h"
+
+using namespace cv;
 int main(int argc, char** argv)
 {
 	(void)argc; (void)argv; // Suppress warnings
@@ -11,5 +17,18 @@ int main(int argc, char** argv)
 	} catch (std::exception& e) {
 		std::cout << "no" << std::endl;
 	}
-	return 0;
+
+        VideoCapture cap;
+        if (argc > 1) cap.open(string(argv[1]));
+        else cap.open(0);
+        Mat frame;
+        namedWindow("video",1);
+        for (;;) {
+            cap >> frame;
+            if(!frame.data) break;
+            GaussianBlur(frame,frame,Size(25,25),6,6);
+            imshow("video", frame);
+            if(waitKey(30) >= 0) break;
+        }
+        return 0;
 }
