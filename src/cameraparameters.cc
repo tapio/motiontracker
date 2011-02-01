7f241bde6b3bb03f1159fe4ba29d4ab5a47fe896
Tapio Vierros <tapiovierros@gmail.com>
2011-02-01 14:15:23 +0200

    Function stubs for CameraParameters.

--- src/cameraparameters.cc
+++ src/cameraparameters.cc
@@ -1,1 +1,19 @@
 #include "motiontracker.hh"
+
+
+CameraParameters::CameraParameters()
+{
+	// TODO
+}
+
+void CameraParameters::saveToFile(std::string filename)
+{
+	// TODO
+}
+
+// static
+CameraParameters CameraParameters::fromFile(std::string filename)
+{
+	// TODO
+	return CameraParameters();
+}
