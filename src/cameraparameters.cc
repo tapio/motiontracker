b7b37a0c349daaccbe38a9af0d0fe32aadd34fbe
Tapio Vierros <tapiovierros@gmail.com>
2011-02-01 20:03:37 +0200

    Fix indentation.

--- src/cameraparameters.cc
+++ src/cameraparameters.cc
@@ -2,19 +2,16 @@
 
 
 CameraParameters::CameraParameters(cv::Mat ip, cv::Mat dc) : intrinsic_parameters(ip), distortion_coeffs(dc)
-{
-
-}
+{ }
 
 void CameraParameters::saveToFile(std::string filename)
 {
 	try {
-	cv::FileStorage fs(filename, cv::FileStorage::WRITE);
-	fs << "mtx" << intrinsic_parameters;
-	fs << "mtx" << distortion_coeffs;
-	fs.release();
-	}
-	catch (cv::Exception& e) {
+		cv::FileStorage fs(filename, cv::FileStorage::WRITE);
+		fs << "mtx" << intrinsic_parameters;
+		fs << "mtx" << distortion_coeffs;
+		fs.release();
+	} catch (cv::Exception& e) {
 		throw std::runtime_error(e.what());
 	}
 }
@@ -28,8 +25,7 @@ CameraParameters CameraParameters::fromFile(std::string filename)
 		fs["mtx"] >> ip;
 		fs["mtx"] >> dc;
 		fs.release();
-	}
-	catch (cv::Exception& e) {
+	} catch (cv::Exception& e) {
 		throw std::runtime_error(e.what());
 	}
 	return CameraParameters(ip, dc);
