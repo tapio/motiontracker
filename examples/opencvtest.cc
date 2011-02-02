fb7c3946cfedfe670e038d82184233112ca7782d
Antti Virta <antti.virta@gmail.com>
2011-02-02 09:44:23 +0200

    SolvePnP test started

--- examples/opencvtest.cc
+++ examples/opencvtest.cc
@@ -11,20 +11,44 @@ struct MyWebcamReceiver: public WebcamListener {
 	std::string window;
 	FPSCounter counter;
 
+	// Test variables
+	float boardScaleFactor; // Chessboard square edge length in units you want to use
+
+	int board_h;
+	int numCorners;
+	Size board_size;
+
+	vector<Point3f> object_corners;
+	vector<Point2f> corners;
+	// End of test variables
+
 	MyWebcamReceiver(Webcam& webcam, std::string win)
-		: WebcamListener(webcam), window(win), counter(5)
-	{}
+		: WebcamListener(webcam), window(win), counter(5), boardScaleFactor(25), board_h(9), numCorners(6*9), board_size(Size(6,9))
+	{
+		for (int i = 0; i < numCorners; ++i) {
+			object_corners.push_back(Point3f(boardScaleFactor*(i / board_h), boardScaleFactor*(i % board_h), 0.0f));
+		}
+	}
 
 	void frameEvent(const cv::Mat &frame) {
+
+		bool patternFound = findChessboardCorners(frame, board_size, corners, CALIB_CB_FAST_CHECK);
+		Mat img = frame;
+		drawChessboardCorners(img, board_size, corners, patternFound);
+
+		/** Old stuff
 		Mat edges;
 		cvtColor(frame, edges, CV_BGR2GRAY);
 		GaussianBlur(edges, edges, Size(15,15), 1.5, 1.5);
 		Canny(edges, edges, 20, 60, 3);
 		putText(edges, boost::lexical_cast<std::string>(counter.getFPS()),
 			Point(0,30), FONT_HERSHEY_PLAIN, 2, CV_RGB(255,0,255));
-		imshow(window, edges);
+		*/
+
+		imshow(window, img);
 		counter();
 	}
+
 };
 
 
