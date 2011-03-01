c3337b83b04fb476b508a99b1b9ac5a77cbbeedc
Tapio Vierros <tapiovierros@gmail.com>
2011-03-01 13:29:42 +0200

    Initial Python API implementation for the Blender plugin.

--- blender/python-module.cc
+++ blender/python-module.cc
@@ -13,9 +13,34 @@
  */
 
 #include <boost/python.hpp>
+#include "pymotiontracker.hh"
+#include "motiontracker.hh"
+#include "utils.hh"
 
 namespace bpy = boost::python;
 
+PyMotionTracker::PyMotionTracker(/*TODO: params*/)
+{
+	webcam = new Webcam;
+	tracker = new ColorCrossTracker(*webcam, 2);
+	// TODO: Handle resource freeing somehow
+}
+
+bpy::tuple PyMotionTracker::getPosition() const
+{
+	if (!tracker) return bpy::make_tuple(0, 0, 0);;
+	cv::Vec3f pos = tracker->getPosition();
+	return bpy::make_tuple(pos[0], pos[1], pos[2]);
+}
+
+bpy::tuple PyMotionTracker::getRotation() const
+{
+	if (!tracker) return bpy::make_tuple(0, 0, 0);;
+	cv::Vec3f rot = tracker->getRotation();
+	return bpy::make_tuple(rot[0], rot[1], rot[2]);
+}
+
+/// Dummy test class
 struct DummyData
 {
 	void set(std::string data) { m_data = data; }
@@ -23,6 +48,7 @@ struct DummyData
 	std::string m_data;
 };
 
+/// Hello world test function
 char const* greet()
 {
 	return "Hello world!";
@@ -37,4 +63,9 @@ BOOST_PYTHON_MODULE(PyMotionTracker)
 	bpy::class_<DummyData>("DummyData")
 		.def("get", &DummyData::get)
 		.def("set", &DummyData::set);
+
+	// The actual MotionTracker API
+	bpy::class_<PyMotionTracker>("PyMotionTracker")//, bpy::init<void /*TODO: constructor params*/>())
+		.def("getPosition", &PyMotionTracker::getPosition)
+		.def("getRotation", &PyMotionTracker::getRotation);
 }
