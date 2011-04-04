fca33539f030dbaf034baa0ffb53cfefd6011bde
Tapio Vierros <tapiovierros@gmail.com>
2011-04-04 11:46:31 +0300

    Some more meat to the blender plugin.

--- blender/blender-plugin.py
+++ blender/blender-plugin.py
@@ -14,18 +14,31 @@ This script utilizes a webcam motion tracker to rotate a blender object.
 '''
 
 from Blender import Draw, Scene
-
+import PyMotionTracker
 
 def main():
+	# Get scene
 	scn = Scene.GetCurrent()
 	if not scn.objects.context:
 		return
 
+	# Create a tracker object and initialize webcam
+	tracker = PyMotionTracker.PyMotionTracker()
+
+	# Get selected objects
 	sel = scn.objects.context
 
-	for ob in sel:
-		ob.loc = (ob.LocX+1,  ob.LocY+1,  ob.LocZ+1) # TODO: Manipulate
-		ob.rot = (ob.RotX+1,  ob.RotY+1,  ob.RotZ+1) # TODO: Manipulate
+	# Main loop
+	while True:
+		# Get location and orientation from the tracker
+		loc = tracker.getPosition()
+		rot = tracker.getRotation()
+
+		# Loop through the objects
+		for ob in sel:
+			# Manipulate object location and orientation
+			ob.loc = (ob.LocX + loc[0],  ob.LocY + loc[1],  ob.LocZ + loc[2])
+			ob.rot = (ob.RotX + rot[0],  ob.RotY + rot[1],  ob.RotZ + rot[2])
 
 	
 if __name__ == '__main__':
