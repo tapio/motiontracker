45bef520c14d0dc4b0d91444f4b37f4567534b0c
Tapio Vierros <tapiovierros@gmail.com>
2011-04-04 13:20:08 +0300

    Blender plugin stuff - in a way it kinda works, partially, almost.

--- blender/motiontracker-blender.py
+++ blender/motiontracker-blender.py
@@ -16,31 +16,48 @@ This script utilizes a webcam motion tracker to rotate a blender object.
 from Blender import Draw, Scene
 import PyMotionTracker
 
-def main():
+# Create a tracker object and initialize webcam
+tracker = PyMotionTracker.PyMotionTracker()
+
+def gui():
+	global tracker
+
 	# Get scene
 	scn = Scene.GetCurrent()
 	if not scn.objects.context:
 		return
 
-	# Create a tracker object and initialize webcam
-	tracker = PyMotionTracker.PyMotionTracker()
-
 	# Get selected objects
 	sel = scn.objects.context
 
-	# Main loop
-	while True:
-		# Get location and orientation from the tracker
-		loc = tracker.getPosition()
-		rot = tracker.getRotation()
+	# Get location and orientation from the tracker
+	loc = tracker.getPosition()
+	rot = tracker.getRotation()
 
-		# Loop through the objects
-		for ob in sel:
-			# Manipulate object location and orientation
-			ob.loc = (ob.LocX + loc[0],  ob.LocY + loc[1],  ob.LocZ + loc[2])
-			ob.rot = (ob.RotX + rot[0],  ob.RotY + rot[1],  ob.RotZ + rot[2])
+	print "Location: " + str(loc)
+	print "Rotation: " + str(rot)
 
-	
+	# Loop through the objects
+	for ob in sel:
+		# Manipulate object location and orientation
+		#ob.loc = (ob.LocX + loc[0],  ob.LocY + loc[1],  ob.LocZ + loc[2])
+		#ob.rot = (ob.RotX + rot[0],  ob.RotY + rot[1],  ob.RotZ + rot[2])
+		ob.rot = (rot[0], rot[1], rot[2])
+
+
+def event(evt, val):
+	if evt == Draw.ESCKEY:
+		Draw.Exit()
+
+
+def bevent( evt ):
+	pass
+
+
+def main():
+	Draw.Register(gui, event, bevent)
+
+
 if __name__ == '__main__':
 	main()
 
