394e3e0c82f9e18c630cb9074123b7637f48e8ec
Antti Virta <antti.virta@gmail.com>
2011-05-03 18:20:23 +0300

    Added filtering to blender plugin and various fixes

--- blender/motiontracker-blender.py
+++ blender/motiontracker-blender.py
@@ -16,14 +16,26 @@ This script utilizes a webcam motion tracker to rotate a blender object.
 from Blender import Draw, Scene, Mathutils
 from Blender.Mathutils import *
 import PyMotionTracker
+from collections import deque
 
 # Create a tracker object and initialize webcam
 tracker = PyMotionTracker.PyMotionTracker()
 
+# Select rotation method between absolute (euler) and rotational
+eulerMethod = False
+
+filterSize = 10
+d = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+meanX = deque(d,filterSize)
+meanY = deque(d,filterSize)
+meanZ = deque(d,filterSize)
+
 def gui():
     global tracker
-    degToRad = 3.1415/180
-
+    global eulerMethod
+    
+    global meanX, meanY, meanZ
+    
     # Get scene
     scn = Scene.GetCurrent()
     if not scn.objects.context:
@@ -35,29 +47,31 @@ def gui():
     # Get location and orientation from the tracker
     loc = tracker.getPosition()
     rot = tracker.getRotation()
-    rotm = tracker.getRotationMatrix()
-    
-    rot_matrix =  Matrix([rotm[0],rotm[1],rotm[2]],
-    [rotm[3],rotm[4],rotm[5]],
-    [rotm[6],rotm[7],rotm[8]])
-    
-    euler = rot_matrix.toEuler()
-    
+        
     rotv = Vector(rot[0], rot[1], rot[2])
     rotMat = RotationMatrix(rotv.length,4,"r",rotv)
+    euler = rotMat.toEuler()
     
+   
+        
     print "Location: " + str(loc)
     print "Euler angles: " + str(euler[0]) + " " + str(euler[1]) + " " + str(euler[2])
 
     # Loop through the objects
     if euler[0] != 0 and euler[1] != 0 and euler[2] != 0:
+         # Add the angles to deques, which manage their size themselves
+        if eulerMethod:
+            meanX.append(euler[0])
+            meanY.append(euler[1])
+            meanZ.append(euler[2])
+            euler = Euler(sum(meanX)/filterSize,sum(meanY)/filterSize,sum(meanZ)/filterSize)
+
+        # Manipulate object location and orientation 
         for ob in sel:
-            # Manipulate object location and orientation
-            # ob.RotX = degToRad*euler[0]
-            # ob.RotY = degToRad*euler[1]
-            # ob.RotZ = degToRad*euler[2]
-            ob.setMatrix(rotMat * ob.getMatrix())
-            # = (degToRad*euler[0], degToRad*euler[1], degToRad*euler[2])
+            if eulerMethod:                 
+                ob.setEuler(euler)
+            else:
+                ob.setMatrix(rotMat * ob.getMatrix())
 
 
 def event(evt, val):
